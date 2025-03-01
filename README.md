2c68bfa5bc8f05a4d5753534664eb839f1d0c783
Victor Renkin <victor.renkin@student.uliege.be>
2025-03-01 09:50:33 +0100

    new maillage with less nodes, maybe do a maillage with a square didn't need is need or not, for the meoment juste use the aillage with less node

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Continuation Sparselizard
 ![ViewCount](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https://github.com/VictorRenkin/continuation_sparselizard&count_bg=%2379C83D&title_bg=%23555555&icon=github.svg&icon_color=%23E7E7E7&title=views&edge_flat=false)
+### Academic Year 2024 – 2025
+
+#### Author: Victor Renkin s2306326
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Requirements](#requirements)
+- [Usage](#usage)
+- [Project Structure](#project-structure)
+- [AI](#ai)
+
+## Introduction
+
+## Requirements  
+This repository works with **Sparselizard**. Follow the documentation available at [this link](https://github.com/halbux/sparselizard-users/tree/main/api/python) to install it properly.  
+
+## Usage
+
+## AI
+The AI is used to occasionally correct the code and to reformulate sentences from the report.
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ loadpoint = 3
 
 fd_rad = 700
 fd     = 30
-mesh = sp.mesh('GMSH/ClampedBeamfiner.msh', 1)
+mesh = sp.mesh('geo_GMSH/ClampedBeam_less_element.msh', 1)
 sp.setfundamentalfrequency(fd) # Set the fundamental frequency exitasion of the structure
 
 
-u = sp.field("h1xyz", [1,2,3,4,5])
+u = sp.field("h1xyz", [1,2,3])
 
 u.setorder(vol, 2)
 
@@ -57,3 +57,26 @@ print("Solving...")
 #     u.write(vol, f"u_f{fd_rad}.vtk", 2, 30)
 #     u.write(vol, f"u_h{fd_rad}.vtk", 2)
 #     break
+
+
+# Exemple of the Newton's law use
+# >>> ...
+# >>> u = field("h1xy", [2,3])
+# >>> u.setorder(sur, 2)
+# >>> acoustics += integral(left, predefinedacousticstructureinteraction(dof(p), tf(p), dof(u), tf(u), 340, 1.2, array2x1(1,0), dbtoneper(500), 1e10)
+
+# Extrude {0, 0, 0.015} {
+#   Point{1}; Point{9}; Point{8}; Point{7}; Point{6}; Point{5}; Point{4}; Point{3}; Point{2}; Curve{4}; Curve{12}; Curve{11}; Curve{10}; Curve{9}; Curve{8}; Curve{7}; Curve{6}; Curve{5}; Curve{3}; Curve{2}; Curve{1}; Surface{4}; Surface{3}; Surface{2}; Surface{1}; Layers{2}; Recombine;
+# }
+# //+
+# Extrude {0, 0, 0.015} {
+#   Curve{30}; Curve{34}; Curve{26}; Curve{38}; Curve{42}; Point{13}; Point{12}; Point{11}; Curve{58}; Curve{62}; Surface{113}; Surface{91}; Point{16}; Point{17}; Point{18}; Curve{46}; Curve{22}; Curve{66}; Surface{135}; Surface{157}; Curve{50}; Curve{54}; Point{15}; Point{14}; Point{10}; Layers{2}; Recombine;
+# }
+# //+
+# Physical Volume(1) = {5, 6, 2, 1, 7, 8, 3, 4};
+# //+
+# Physical Surface(2) = {276, 57, 262, 53, 165, 173, 41, 37};
+# //+
+# Physical Point(3) = {17};
+# //+
+# Show "*";
